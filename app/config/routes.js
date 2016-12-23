ffd762fa9eefd82cacceccf3afc71a19d40c2a00
Ernesto Ramirez <ernesto.ramirez88@gmail.com>
2016-12-22 16:05:28 -0800

    added base state/props for profile -> user/repos/notes

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,11 +3,13 @@ var Router     = require('react-router');
 var Route      = Router.Route;
 var IndexRoute = Router.IndexRoute;
 
-var Main = require('../components/Main')
-var Home = require('../components/Home')
+var Main    = require('../components/Main')
+var Home    = require('../components/Home')
+var Profile = require('../components/Profile')
 
 module.exports = (
 	<Route path="/" component={Main}>
+		<Route path="profile/:username" component={Profile}/>
 		<IndexRoute component={Home}/>
 	</Route>
 );
